ee225f22ecdf89d53d0d31f5e51cae8c0ba6c512
Erick <erickbarretodasilva@gmail.com>
2024-10-26 16:28:10 -0300

    Projeto para estudo da linguagem java do básico ao avançado.

---
+++ src/Estudos.java
@@ -0,0 +1,12 @@
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("JVM (Java Virtual Machine) - é a Máquina Virtual do Java responsável por executar o bytecode");
+        System.out.println("JRE (Java Runtime Environment) - Ambiente de Execução do Java que fornece as bibliotecas padrões do Java para o JDK compilar o seu código e para a JVM executar o seu program");
+        // digitar sout
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 9.1;
+    }
+}
