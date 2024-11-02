e481ac4b4d5728a42528f33c00f4e69557220b84
Erick <erickbarretodasilva@gmail.com>
2024-11-02 12:18:31 -0300

    fim do curso

--- src/Loop.java
+++ src/Loop.java
@@ -5,6 +5,7 @@ public class Loop {
         Scanner leitura = new Scanner(System.in);
         double mediaAvaliacao = 0;
         double nota = 0;
+        int totalDeNotas = 0;
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Diga sua avaliação para o filme  ");
@@ -14,5 +15,20 @@ public class Loop {
 
         System.out.println("Média de avaliações " + mediaAvaliacao/3);
 
+        while (nota != -1) {
+            System.out.println("Diga sua avaliação para o filme ou -1 para encerrar  ");
+            nota = leitura.nextDouble();
+
+            if (nota != -1) {
+                mediaAvaliacao +=  nota;
+                totalDeNotas++;
+            }
+
+        }
+
+        System.out.println("Média de avaliações " + mediaAvaliacao / totalDeNotas);
+
     }
+
+
 }
